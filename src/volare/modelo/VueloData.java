b16f980ca7ae541ea1bae0b9a09e0dc14ed6151f
nakrull11 <gusguz1994@gmail.com>
2018-12-04 16:08:06 -0300

    modificaciones varias VueloData

---
+++ src/volare/modelo/VueloData.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volare.modelo;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author gustavo
+ */
+public class VueloData extends Data {
+    
+    public VueloData(Conexion conexion) {
+        super(conexion);
+    }
+    
+    /*public Vuelo consultarVuelos(Ciudad ciudadDestino, Date fecha){
+        
+    
+    }*/
+    
+}
